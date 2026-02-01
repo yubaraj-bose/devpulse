b7242f321add4b9a0df801d07155d70fb850dd4f
sankarsharn <sankarsharnrastogi@gmail.com>
2026-02-02 02:18:10 +0530

    fixed errors

---
+++ DevPulse/frontend/web/src/lib/prisma.js
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+const globalForPrisma = globalThis;
+
+// This ensures we reuse the same client across reloads
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
