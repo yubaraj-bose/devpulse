72e1231c843a05c585d966d78e69335a428e4c3d
yubaraj-bose <156693378+yubaraj-bose@users.noreply.github.com>
2026-02-10 00:29:33 +0530

    Revert "Yubaraj"

--- DevPulse/frontend/web/src/app/layout.js
+++ DevPulse/frontend/web/src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import NavBarClient from "./components/NavBarClient";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +26,6 @@ export default function RootLayout({ children }) {
   afterSignInUrl="/dashboard"
   afterSignUpUrl="/dashboard"
 >
-  <NavBarClient/>
   {children}
 </ClerkProvider>
 
